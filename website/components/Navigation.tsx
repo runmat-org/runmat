294365a4d4d5ebed7fcf463450f5a95f594193b1
Nabeel Allana <nallana@dystr.com>
2025-08-08 18:03:00 -0700

    updating logo / icons

--- website/components/Navigation.tsx
+++ website/components/Navigation.tsx
@@ -24,7 +24,40 @@ export default function Navigation() {
       <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-6">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <div className="h-6 w-6 rounded" style={{background: 'linear-gradient(135deg, #3ea7fd 0%, #bb51ff 100%)'}} />
+            <svg height="20" viewBox="0 0 264 134" fill="none" xmlns="http://www.w3.org/2000/svg" className="pr-2">
+              <path d="M30 33H0V101H30V33Z" fill="url(#paint0_linear_0_1)" />
+              <path d="M264 33H230V101H264V33Z" fill="url(#paint1_linear_0_1)" />
+              <path d="M30 33V0H96V33H30Z" fill="url(#paint2_linear_0_1)" />
+              <path d="M165 134V101H230V134H165Z" fill="url(#paint3_linear_0_1)" />
+              <path d="M96 65V33H132V65H96Z" fill="url(#paint4_linear_0_1)" />
+              <path d="M132 101V66H165V101H132Z" fill="url(#paint5_linear_0_1)" />
+              <defs>
+                <linearGradient id="paint0_linear_0_1" x1="15" y1="33" x2="15" y2="101" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#CA26D6" />
+                  <stop offset="1" stopColor="#D500C6" />
+                </linearGradient>
+                <linearGradient id="paint1_linear_0_1" x1="247" y1="33" x2="247" y2="101" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#CA26D6" />
+                  <stop offset="1" stopColor="#D500C6" />
+                </linearGradient>
+                <linearGradient id="paint2_linear_0_1" x1="63" y1="0" x2="63" y2="33" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#CA26D6" />
+                  <stop offset="1" stopColor="#D500C6" />
+                </linearGradient>
+                <linearGradient id="paint3_linear_0_1" x1="197.5" y1="101" x2="197.5" y2="134" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#CA26D6" />
+                  <stop offset="1" stopColor="#D500C6" />
+                </linearGradient>
+                <linearGradient id="paint4_linear_0_1" x1="114" y1="33" x2="114" y2="65" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#CA26D6" />
+                  <stop offset="1" stopColor="#D500C6" />
+                </linearGradient>
+                <linearGradient id="paint5_linear_0_1" x1="148.5" y1="66" x2="148.5" y2="101" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#CA26D6" />
+                  <stop offset="1" stopColor="#D500C6" />
+                </linearGradient>
+              </defs>
+            </svg>
             <span className="hidden font-bold sm:inline-block">RunMat</span>
           </Link>
           <NavigationMenu>
