af54759542174c1d8ef1d2133df0b121537b067b
Nabeel Allana <nallana@dystr.com>
2026-04-13 14:03:52 -0700

    moving over bytecode compile

--- crates/runmat-vm/src/lib.rs
+++ crates/runmat-vm/src/lib.rs
@@ -4,13 +4,21 @@ pub mod accel;
 pub mod bytecode;
 pub mod call;
 pub mod compiler;
+pub mod functions {
+    pub use crate::bytecode::program::{Bytecode, CallFrame, ExecutionContext, UserFunction};
+}
 pub mod indexing;
+pub mod instr {
+    pub use crate::bytecode::instr::{ArgSpec, EmitLabel, EndExpr, Instr, StackEffect};
+}
 pub mod interpreter;
 pub mod object;
 pub mod ops;
 pub mod runtime;
 
+pub use bytecode::compile;
 pub use bytecode::{
     ArgSpec, Bytecode, CallFrame, EmitLabel, EndExpr, ExecutionContext, Instr, StackEffect,
     UserFunction,
 };
+pub use compiler::CompileError;
