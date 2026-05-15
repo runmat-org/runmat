d4cd70e494a2143cf8ba89f53844cf3939506a5d
Nabeel Allana <nallana@dystr.com>
2026-05-15 13:41:54 -0700

    RM-378: convert Turbine hot callback

--- crates/runmat-turbine/tests/jit.rs
+++ crates/runmat-turbine/tests/jit.rs
@@ -2022,72 +2022,37 @@ fn test_jit_function_compilation_performance() {
 
     let mut engine = TurbineEngine::new().expect("Failed to create engine");
 
-    // Create a computationally intensive function
-    let mut functions = HashMap::new();
-    functions.insert(
-        "compute_intensive".to_string(),
-        runmat_vm::legacy::LegacyUserFunction {
-            name: "compute_intensive".to_string(),
-            params: vec![runmat_hir::VarId(0)],
-            outputs: vec![runmat_hir::VarId(1)],
-            body: vec![
-                // temp = x * x
-                runmat_hir::HirStmt::Assign(
-                    runmat_hir::VarId(2),
-                    runmat_hir::HirExpr {
-                        kind: runmat_hir::HirExprKind::Binary(
-                            Box::new(runmat_hir::HirExpr {
-                                kind: runmat_hir::HirExprKind::Var(runmat_hir::VarId(0)),
-                                ty: Type::Num,
-                                span: runmat_hir::Span::default(),
-                            }),
-                            runmat_parser::BinOp::Mul,
-                            Box::new(runmat_hir::HirExpr {
-                                kind: runmat_hir::HirExprKind::Var(runmat_hir::VarId(0)),
-                                ty: Type::Num,
-                                span: runmat_hir::Span::default(),
-                            }),
-                        ),
-                        ty: Type::Num,
-                        span: runmat_hir::Span::default(),
-                    },
-                    false, // Assignment suppression flag for test
-                    runmat_hir::Span::default(),
-                ),
-                // result = temp + temp
-                runmat_hir::HirStmt::Assign(
-                    runmat_hir::VarId(1),
-                    runmat_hir::HirExpr {
-                        kind: runmat_hir::HirExprKind::Binary(
-                            Box::new(runmat_hir::HirExpr {
-                                kind: runmat_hir::HirExprKind::Var(runmat_hir::VarId(2)),
-                                ty: Type::Num,
-                                span: runmat_hir::Span::default(),
-                            }),
-                            runmat_parser::BinOp::Add,
-                            Box::new(runmat_hir::HirExpr {
-                                kind: runmat_hir::HirExprKind::Var(runmat_hir::VarId(2)),
-                                ty: Type::Num,
-                                span: runmat_hir::Span::default(),
-                            }),
-                        ),
-                        ty: Type::Num,
-                        span: runmat_hir::Span::default(),
-                    },
-                    false, // Assignment suppression flag for test
-                    runmat_hir::Span::default(),
-                ),
-            ],
-            local_var_count: 3,
-            has_varargin: false,
-            has_varargout: false,
-            var_types: Vec::new(),
+    let function = runmat_hir::FunctionId(1);
+    let mut semantic_functions = HashMap::new();
+    semantic_functions.insert(
+        function,
+        SemanticFunctionBytecode {
+            function,
+            display_name: "compute_intensive".to_string(),
             source_id: None,
+            instructions: vec![
+                Instr::LoadVar(0),
+                Instr::LoadVar(0),
+                Instr::Mul,
+                Instr::StoreVar(2),
+                Instr::LoadVar(2),
+                Instr::LoadVar(2),
+                Instr::Add,
+                Instr::StoreVar(1),
+            ],
+            instr_spans: Vec::new(),
+            call_arg_spans: Vec::new(),
+            var_count: 3,
+            input_slots: vec![0],
+            varargin_slot: None,
+            output_slots: vec![1],
+            varargout_slot: None,
+            capture_slots: Vec::new(),
         },
     );
 
     let bytecode = Bytecode {
-        functions,
+        semantic_functions,
         ..Bytecode::with_instructions(
             vec![
                 Instr::LoadConst(6.0),
