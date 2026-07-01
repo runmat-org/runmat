bfac57ef7220821d79f0a8a607570df54a68cd45
Nabeel Allana <nallana@dystr.com>
2026-07-01 14:20:27 -0700

    RM-544: add realsqrt builtin

--- crates/runmat-runtime/src/builtins/math/elementwise/mod.rs
+++ crates/runmat-runtime/src/builtins/math/elementwise/mod.rs
@@ -27,6 +27,7 @@ pub(crate) mod pow2;
 pub(crate) mod power;
 pub(crate) mod rdivide;
 pub(crate) mod real;
+pub(crate) mod realsqrt;
 pub(crate) mod rescale;
 pub(crate) mod sign;
 pub(crate) mod single;
